f1a8d7ce2e878b0117d7d13f260b7111b9951591
jnorthrup <jnorthrup@example.com>
2026-01-17 16:29:01 -0600

    feat: expose QuickBooks mobile view, harmonize TCC format, and refine account graph layout

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 
 export const TIN_PLACEHOLDER = 'XX-XXXXXXX';
-export const TCC_PLACEHOLDER = 'T1234567890';
+export const TCC_PLACEHOLDER = 'XX-XXXXXXX';
 export const SSN_PLACEHOLDER = 'XXX-XX-XXXX';
 export const ACCOUNT_PLACEHOLDER = 'XXXX-XXXX';
 export const PENDING_STATUS = 'PENDING';
@@ -12,4 +12,5 @@ export const DEFAULT_VITE_HOST = '0.0.0.0';
 
 export const REGEX_EIN = /^\d{2}-\d{7}$/;
 export const REGEX_SSN = /^\d{3}-\d{2}-\d{4}$/;
+export const REGEX_TCC = /^[A-Z]{2}-[A-Z0-9]{7}$/;
 export const REGEX_DIGITS_ONLY = /\D/g;
