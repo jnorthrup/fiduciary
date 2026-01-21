e72648cdf90b5909d441bc6238dcf8ec11820aaf
jnorthrup <jnorthrup@example.com>
2026-01-21 16:19:38 -0600

    Deploy: 2026-01-21 16:19:38

--- old/generated/api-client/models/TINValidationBatch.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { TINValidationSingle } from './TINValidationSingle';
-export type TINValidationBatch = {
-    requests: Array<TINValidationSingle>;
-};
-
--- old/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Hooks Index
- * Re-exports all custom hooks for easy importing
- */
-
-export { useWizard, useSimpleWizard } from './useWizard';
-export type { WizardStep, UseWizardOptions, UseWizardReturn } from './useWizard';
